18f2925c4370a3063377fbef31f2d9fa2877e4fb
Milan Panta <milan.panta@icloud.com>
2023-10-19 20:25:30 -0400

    set cursor to block

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -7,6 +7,8 @@ vim.opt.relativenumber = true
 vim.opt.number = true
 vim.opt.statuscolumn = "%=%{&nu?(&rnu && v:relnum?v:relnum:v:lnum):''} %C"
 
+vim.opt.guicursor = ""
+
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2

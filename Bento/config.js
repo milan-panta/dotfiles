cba49d9c19da1b33295606781625809233bbd9db
Milan Panta <milan.panta@icloud.com>
2023-10-25 23:59:42 -0400

    removed name from greeting

--- Bento/config.js
+++ Bento/config.js
@@ -17,9 +17,9 @@ const CONFIG = {
   twelveHourFormat: false,
 
   // Greetings
-  greetingMorning: "Good morning!",
-  greetingAfternoon: "Good afternoon,",
-  greetingEvening: "Good evening,",
+  greetingMorning: "Good Morning!",
+  greetingAfternoon: "Good Afternoon!",
+  greetingEvening: "Good Evening!",
   greetingNight: "Go to Sleep!",
 
   // Layout

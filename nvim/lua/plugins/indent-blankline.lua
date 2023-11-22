82f81852f83c92adaba8651c4e08364c2382119a
Milan Panta <milan.panta@icloud.com>
2023-11-22 18:50:20 -0500

    improved indent-blankline

---
+++ nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,57 @@
+return {
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {
+      indent = {
+        char = "│",
+        tab_char = "│",
+      },
+      scope = { enabled = false },
+      exclude = {
+        filetypes = {
+          "help",
+          "neo-tree",
+          "Trouble",
+          "trouble",
+          "lazy",
+          "mason",
+          "toggleterm",
+        },
+      },
+    },
+    main = "ibl",
+  },
+
+  {
+    "echasnovski/mini.indentscope",
+    version = false,
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+      require("mini.indentscope").setup({
+        symbol = "│",
+        options = { try_as_border = true },
+        draw = {
+          delay = 0,
+          animation = require("mini.indentscope").gen_animation.none(),
+        },
+      })
+    end,
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = {
+          "help",
+          "neo-tree",
+          "Trouble",
+          "trouble",
+          "lazy",
+          "mason",
+          "toggleterm",
+        },
+        callback = function()
+          vim.b.miniindentscope_disable = true
+        end,
+      })
+    end,
+  },
+}

f52bdfdabaa3cb793218326f4f6c2ff6caddcf30
Milan Panta <milan.panta@icloud.com>
2024-07-23 03:07:29 -0700

    restructuring colorscheme, cleanup

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -68,7 +68,7 @@ function RunFile(dir)
   end
   vim.cmd(dir)
   if filetype == "cpp" then
-    vim.cmd("term clang++ -std=c++17 -Wall % -o %< && ./%<")
+    vim.cmd("term g++ -std=c++20 -Wall % -o %< && ./%<")
     vim.fn.feedkeys("i")
     return
   elseif filetype == "go" then

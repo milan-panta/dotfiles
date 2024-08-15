cbfc4396d4dc380d21ce0e7859ac9db1768630f1
Milan Panta <milan.panta@icloud.com>
2024-08-15 17:38:33 +0200

    qol

--- nvim/init.lua
+++ nvim/init.lua
@@ -11,7 +11,6 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  plugins = { install = { colorscheme = { "kanagawa-dragon" } } },
   spec = {
     { import = "plugins" },
   },
--- nvim/lua/plugins/debug.lua
+++ nvim/lua/plugins/debug.lua
@@ -1,102 +1,64 @@
 return {
-  "mfussenegger/nvim-dap",
-  keys = {
-    {
-      "<Leader>db",
-      function()
-        vim.cmd("DapToggleBreakpoint")
-      end,
-      desc = "Toggle breakpoint",
-    },
-    {
-      "<Leader>dc",
-      function()
-        require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
-      end,
-      desc = "Toggle conditional breakpoint",
-    },
-    {
-      "<Leader>dl",
-      function()
-        require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
-      end,
-      desc = "Toggle breakpoint with log",
-    },
-  },
-  dependencies = {
-    {
-      "nvim-neotest/nvim-nio",
-    },
-    {
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
       "rcarriga/nvim-dap-ui",
-      dependencies = "mfussenegger/nvim-dap",
-    },
-    {
-      "mfussenegger/nvim-dap-python",
-      config = function()
-        local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
-        require("dap-python").setup(path)
-      end,
-    },
-    {
-      "jay-babu/mason-nvim-dap.nvim",
-    },
-    {
-      "LiadOz/nvim-dap-repl-highlights",
-      opts = {},
-    },
-    {
       "theHamsta/nvim-dap-virtual-text",
-      config = function()
-        require("nvim-dap-virtual-text").setup({
-          -- virt_text_pos = vim.fn.has("nvim-0.10") == 1 and "inline" or "eol",
-          virt_text_pos = "eol",
-        })
-      end,
+      "nvim-neotest/nvim-nio",
+      "williamboman/mason.nvim",
     },
-  },
-  config = function()
-    -- debugging
-    require("mason-nvim-dap").setup({
-      handlers = {},
-      ensure_installed = { --[[ "python", ]]
-        "codelldb",
-      },
-      automatic_installation = true,
-    })
-    vim.keymap.set("n", "<F5>", function()
-      require("dap").continue()
-    end, { desc = "Continue debugging" })
-    vim.keymap.set("n", "<F3>", function()
-      require("dap").step_into()
-    end, { desc = "Step into" })
-    vim.keymap.set("n", "<F9>", function()
-      require("dap").terminate()
-    end, { desc = "Terminate debugging" })
-    vim.keymap.set("n", "<F4>", function()
-      require("dap").step_over()
-    end, { desc = "Step over" })
-    vim.keymap.set("n", "<F12>", function()
-      require("dap").step_out()
-    end, { desc = "Step out" })
-    vim.keymap.set("n", "<F6>", function()
-      require("dap").repl.open()
-    end, { desc = "Open new debuging repl" })
-    vim.keymap.set("n", "<Leader>dp", function()
-      require("dap-python").test_method()
-    end, { desc = "Start Python Debugging" })
+    event = {"BufReadPost", "BufNewFile"},
+    config = function()
+      local dap = require "dap"
+      local ui = require "dapui"
 
-    local dap = require("dap")
-    local dapui = require("dapui")
-    dapui.setup()
-    dap.listeners.after.event_initialized["dapui_config"] = function()
-      dapui.open()
-    end
-    dap.listeners.before.event_terminated["dapui_config"] = function()
-      dapui.close()
-    end
-    dap.listeners.before.event_exited["dapui_config"] = function()
-      dapui.close()
-    end
-  end,
+      require("dapui").setup()
+
+      require("nvim-dap-virtual-text").setup {
+        -- This just tries to mitigate the chance that I leak tokens here. Probably won't stop it from happening...
+        display_callback = function(variable)
+          local name = string.lower(variable.name)
+          local value = string.lower(variable.value)
+          if name:match "secret" or name:match "api" or value:match "secret" or value:match "api" then
+            return "*****"
+          end
+
+          if #variable.value > 15 then
+            return " " .. string.sub(variable.value, 1, 15) .. "... "
+          end
+
+          return " " .. variable.value
+        end,
+      }
+
+
+      vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
+      vim.keymap.set("n", "<space>gb", dap.run_to_cursor)
+
+      -- Eval var under cursor
+      vim.keymap.set("n", "<space>?", function()
+        require("dapui").eval(nil, { enter = true })
+      end)
+
+      vim.keymap.set("n", "<F1>", dap.continue)
+      vim.keymap.set("n", "<F2>", dap.step_into)
+      vim.keymap.set("n", "<F3>", dap.step_over)
+      vim.keymap.set("n", "<F4>", dap.step_out)
+      vim.keymap.set("n", "<F5>", dap.step_back)
+      vim.keymap.set("n", "<F13>", dap.restart)
+
+      dap.listeners.before.attach.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        ui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        ui.close()
+      end
+    end,
+  },
 }

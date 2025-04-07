4bc7097f623fcb3a598f6d4cd9c5ccec702633cc
Milan Panta <milan.panta@icloud.com>
2025-04-07 16:50:14 -0400

    qol

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -11,8 +11,8 @@ return {
     event = { "BufReadPost", "BufNewFile" },
     config = function()
       local dap = require("dap")
-      local ui = require("dapui")
-      require("dapui").setup()
+      local dapui = require("dapui")
+      dapui.setup()
       require("nvim-dap-virtual-text").setup()
 
       -- debugging
@@ -45,18 +45,11 @@ return {
       vim.keymap.set("n", "<F5>", dap.step_back)
       vim.keymap.set("n", "<F12>", dap.restart)
 
-      local dap, dapui = require("dap"), require("dapui")
       dap.listeners.before.attach.dapui_config = function()
         dapui.open()
       end
       dap.listeners.before.launch.dapui_config = function()
         dapui.open()
-      end
-      dap.listeners.before.event_terminated.dapui_config = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited.dapui_config = function()
-        dapui.close()
       end
     end,
   },

f64316ea3b5c89b7f5e94243c5e18e038481372b
Milan Panta <milan.panta@icloud.com>
2025-08-17 02:30:59 -0700

    qol

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,3 +1,4 @@
+-- lua/plugins/telescope.lua
 return {
   "nvim-telescope/telescope.nvim",
   dependencies = {
@@ -5,48 +6,7 @@ return {
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     "debugloop/telescope-undo.nvim",
   },
-  config = function()
-    local telescope = require("telescope")
-    telescope.setup({
-      defaults = {
-        mappings = {
-          i = {
-            ["<ESC>"] = require("telescope.actions").close,
-          },
-        },
-        history = { cycle_wrap = true },
-        prompt_prefix = " \239\128\130  ",
-        selection_caret = "  ",
-        entry_prefix = "  ",
-      },
-      pickers = {
-        buffers = { theme = "ivy", previewer = false },
-        oldfiles = { theme = "ivy", previewer = false },
-        find_files = {
-          theme = "ivy",
-          hidden = true,
-          find_command = {
-            "rg",
-            "--files",
-            "--glob",
-            "!.git",
-            "--glob",
-            "!venv",
-            "--glob",
-            "!_build",
-            "--hidden",
-          },
-          previewer = false,
-        },
-      },
-      extensions = { fzf = {} },
-    })
-    pcall(require("telescope").load_extension, "fzf")
-    require("telescope").load_extension("undo")
-  end,
-  cmd = {
-    "Telescope",
-  },
+  cmd = { "Telescope" },
   keys = {
     {
       "<Leader><Leader>",
@@ -140,4 +100,97 @@ return {
       desc = "Telescope Config",
     },
   },
+  config = function()
+    local telescope = require("telescope")
+    local actions = require("telescope.actions")
+    local undo_actions = require("telescope-undo.actions")
+
+    telescope.setup({
+      defaults = {
+        initial_mode = "insert",
+        mappings = {
+          i = {
+            ["<ESC>"] = actions.close,
+          },
+        },
+        layout_strategy = "flex",
+        layout_config = {
+          width = 0.96,
+          height = 0.94,
+          prompt_position = "top",
+          preview_cutoff = 110, -- switch to vertical below this width
+          horizontal = {
+            preview_width = 0.58, -- results get the remainder
+          },
+          vertical = {
+            preview_height = 0.52,
+          },
+        },
+        sorting_strategy = "ascending",
+        history = { cycle_wrap = true },
+        prompt_prefix = " \239\128\130  ",
+        selection_caret = "  ",
+        entry_prefix = "  ",
+      },
+
+      pickers = {
+        buffers = { theme = "ivy", previewer = false },
+        oldfiles = { theme = "ivy", previewer = false },
+        find_files = {
+          theme = "ivy",
+          hidden = true,
+          find_command = {
+            "rg",
+            "--files",
+            "--glob",
+            "!.git",
+            "--glob",
+            "!venv",
+            "--glob",
+            "!_build",
+            "--hidden",
+          },
+          previewer = false,
+        },
+      },
+
+      extensions = {
+        fzf = {},
+        undo = {
+          use_delta = true,
+          side_by_side = true,
+          layout_strategy = "flex",
+          layout_config = {
+            width = 0.96,
+            height = 0.94,
+            prompt_position = "top",
+            preview_cutoff = 110,
+            horizontal = {
+              preview_width = 0.62, -- give diff preview a bit more space
+            },
+            vertical = {
+              preview_height = 0.55,
+            },
+          },
+          mappings = {
+            i = {
+              ["<CR>"] = undo_actions.restore,
+              ["<C-a>"] = undo_actions.yank_additions,
+              ["<C-d>"] = undo_actions.yank_deletions,
+              ["<C-y>"] = undo_actions.yank_state,
+            },
+            n = {
+              ["<CR>"] = undo_actions.restore,
+              ["a"] = undo_actions.yank_additions,
+              ["d"] = undo_actions.yank_deletions,
+              ["y"] = undo_actions.yank_state,
+            },
+          },
+        },
+      },
+    })
+
+    pcall(telescope.load_extension, "fzf")
+    telescope.load_extension("undo")
+  end,
 }

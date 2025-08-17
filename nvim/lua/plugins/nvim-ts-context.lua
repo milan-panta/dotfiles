9a83daa327993e77e8f48c90248ce157097f8e94
Milan Panta <milan.panta@icloud.com>
2025-08-17 02:21:33 -0700

    fix weird ts-context interaction with telescope undo

--- nvim/lua/plugins/nvim-ts-context.lua
+++ nvim/lua/plugins/nvim-ts-context.lua
@@ -2,8 +2,22 @@ return {
   "nvim-treesitter/nvim-treesitter-context",
   event = { "BufReadPost", "BufNewFile" },
   config = function()
+    local tc = require("treesitter-context")
+
+    tc.setup({
+      -- only attach when it's a normal buffer, not Telescope
+      on_attach = function(buf)
+        local bt = vim.api.nvim_get_option_value("buftype", { buf = buf })
+        local ft = vim.api.nvim_get_option_value("filetype", { buf = buf })
+        if bt == "prompt" or ft == "TelescopePrompt" or ft == "TelescopeResults" then
+          return false
+        end
+        return true
+      end,
+    })
+
     vim.keymap.set("n", "[w", function()
-      require("treesitter-context").go_to_context()
-    end, { silent = true })
+      tc.go_to_context()
+    end, { silent = true, desc = "Treesitter Context: jump up" })
   end,
 }

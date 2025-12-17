e8076cb9e164dcfde380c1b96052ff540f42604e
Milan Panta <milan.panta@icloud.com>
2025-12-17 14:07:01 -0500

    fix

--- nvim/lua/plugins/lintfmt.lua
+++ nvim/lua/plugins/lintfmt.lua
@@ -24,6 +24,12 @@ return {
       local conform = require("conform")
 
       conform.setup({
+        format_on_save = function(bufnr)
+          if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+            return
+          end
+          return { timeout_ms = 500, lsp_fallback = true }
+        end,
         formatters_by_ft = {
           c = { "clang_format" },
           cpp = { "clang_format" },

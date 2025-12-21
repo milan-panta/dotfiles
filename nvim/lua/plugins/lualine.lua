e6bea341cac3f970a4a412022906b9b8f7803c6d
Milan Panta <milan.panta@icloud.com>
2025-12-20 19:17:29 -0500

    revamp lualine config to be more minilalist

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -10,27 +10,27 @@ return {
       theme = {
         normal = {
           a = { fg = "#a9b1d6", bg = "#16161e", gui = "bold" },
-          b = { fg = "#565f89", bg = "#16161e" },
+          b = { fg = "#bb9af7", bg = "#16161e" },
           c = { fg = "#565f89", bg = "#16161e" },
         },
         insert = {
           a = { fg = "#9ece6a", bg = "#16161e", gui = "bold" },
-          b = { fg = "#565f89", bg = "#16161e" },
+          b = { fg = "#bb9af7", bg = "#16161e" },
           c = { fg = "#565f89", bg = "#16161e" },
         },
         visual = {
           a = { fg = "#e0af68", bg = "#16161e", gui = "bold" },
-          b = { fg = "#565f89", bg = "#16161e" },
+          b = { fg = "#bb9af7", bg = "#16161e" },
           c = { fg = "#565f89", bg = "#16161e" },
         },
         replace = {
           a = { fg = "#f7768e", bg = "#16161e", gui = "bold" },
-          b = { fg = "#565f89", bg = "#16161e" },
+          b = { fg = "#bb9af7", bg = "#16161e" },
           c = { fg = "#565f89", bg = "#16161e" },
         },
         command = {
-          a = { fg = "#7dcfff", bg = "#16161e", gui = "bold" },
-          b = { fg = "#565f89", bg = "#16161e" },
+          a = { fg = "#e0af68", bg = "#16161e", gui = "bold" },
+          b = { fg = "#bb9af7", bg = "#16161e" },
           c = { fg = "#565f89", bg = "#16161e" },
         },
         inactive = {
@@ -52,7 +52,7 @@ return {
         { "filetype", icons_enabled = false },
       },
       lualine_y = { "progress" },
-      lualine_z = { "location" },
+      lualine_z = { { "location", color = { fg = "#a9b1d6", bg = "#16161e", gui = "bold" } } },
     },
   },
 }

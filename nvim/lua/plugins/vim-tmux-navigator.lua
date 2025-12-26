6a6fb23160dba3e0c12d8abe6338d088b62a8162
Milan Panta <milan.panta@icloud.com>
2025-12-25 23:46:25 -0500

    use cmmd key for tmux navigation

--- nvim/lua/plugins/vim-tmux-navigator.lua
+++ nvim/lua/plugins/vim-tmux-navigator.lua
@@ -9,10 +9,9 @@ return {
     "TmuxNavigatorProcessList",
   },
   keys = {
-    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    { "<M-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+    { "<M-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+    { "<M-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+    { "<M-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
   },
 }

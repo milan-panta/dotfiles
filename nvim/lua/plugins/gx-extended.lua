42d401737ba6b822cd974e230a900c28afaf0c0c
Milan Panta <milan.panta@icloud.com>
2026-01-27 21:52:00 -0500

    update mpv config

--- nvim/lua/plugins/gx-extended.lua
+++ nvim/lua/plugins/gx-extended.lua
@@ -2,6 +2,10 @@ return {
   "rmagatti/gx-extended.nvim",
   keys = { "gx" },
   config = function()
-    require("gx-extended").setup({})
+    require("gx-extended").setup({
+      open_fn = function(url)
+        vim.ui.open(url)
+      end,
+    })
   end,
 }
--- nvim/lua/plugins/yazi.lua
+++ nvim/lua/plugins/yazi.lua
@@ -1,20 +1,29 @@
 return {
   "mikavilpas/yazi.nvim",
   version = "*",
+  event = "VeryLazy",
   dependencies = {
     { "nvim-lua/plenary.nvim", lazy = true },
   },
   keys = {
-    {
-      "-",
-      mode = { "n", "v" },
-      "<cmd>Yazi<cr>",
-      desc = "Open yazi at the current file",
-    },
-    {
-      "<leader>cw",
-      "<cmd>Yazi cwd<cr>",
-      desc = "Open the file manager in nvim's working directory",
+    { "<leader>ll", mode = { "n", "v" }, "<cmd>Yazi<cr>", desc = "Open yazi at the current file" },
+    { "<leader>lL", "<cmd>Yazi cwd<cr>", desc = "Open the file manager in nvim's working directory" },
+    { "<leader>ly", "<cmd>Yazi toggle<cr>", desc = "Resume last yazi session" },
+  },
+  ---@type YaziConfig
+  opts = {
+    open_for_directories = true,
+    integrations = {
+      grep_in_directory = function(directory)
+        Snacks.picker.grep({ cwd = directory })
+      end,
+      grep_in_selected_files = function(selected_files)
+        Snacks.picker.grep({ dirs = selected_files })
+      end,
     },
   },
+  init = function()
+    -- Disable netrw so yazi opens for directories
+    vim.g.loaded_netrwPlugin = 1
+  end,
 }

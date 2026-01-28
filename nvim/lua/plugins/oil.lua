6542521fd1b2e8fb433aa3dc3c3f1b79b6ee6c93
Milan Panta <milan.panta@shopify.com>
2026-01-28 16:10:38 -0500

    Add vimium-c CSS and fix navigation

---
+++ nvim/lua/plugins/oil.lua
@@ -0,0 +1,14 @@
+return {
+  "stevearc/oil.nvim",
+  cmd = "Oil",
+  keys = {
+    { "-", "<cmd>Oil<cr>", desc = "Open Oil" },
+  },
+  dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+  opts = {
+    default_file_explorer = true,
+    view_options = {
+      show_hidden = true,
+    },
+  },
+}
--- nvim/lua/plugins/yazi.lua
+++ nvim/lua/plugins/yazi.lua
@@ -1,20 +1,23 @@
 return {
   "mikavilpas/yazi.nvim",
-  version = "*",
+  keys = {
+    { "<leader>-", mode = { "n", "v" }, "<cmd>Yazi<cr>", desc = "Open yazi at the current file" },
+    { "<leader>_", "<cmd>Yazi cwd<cr>", desc = "Open the file manager in nvim's working directory" },
+    { "<leader>cw", "<cmd>Yazi toggle<cr>", desc = "Resume last yazi session" },
+  },
   dependencies = {
     { "nvim-lua/plenary.nvim", lazy = true },
   },
-  keys = {
-    {
-      "-",
-      mode = { "n", "v" },
-      "<cmd>Yazi<cr>",
-      desc = "Open yazi at the current file",
-    },
-    {
-      "<leader>cw",
-      "<cmd>Yazi cwd<cr>",
-      desc = "Open the file manager in nvim's working directory",
+  ---@type YaziConfig
+  opts = {
+    open_for_directories = false,
+    integrations = {
+      grep_in_directory = function(directory)
+        Snacks.picker.grep({ cwd = directory })
+      end,
+      grep_in_selected_files = function(selected_files)
+        Snacks.picker.grep({ dirs = selected_files })
+      end,
     },
   },
 }

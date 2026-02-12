9ba1c68d0f995d73c19f199bad327a1d14f1b126
Milan Panta <milan.panta@icloud.com>
2026-02-12 08:50:57 -0700

    Add neovim style pane swapping to tmux
    Fix sidekick integration for changeed files

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -71,7 +71,7 @@ local function RunFile(dir, args)
     cmd = string.format("gcc -Wall -g -std=gnu99 '%s' -o '%s' && '%s' %s", file, file_no_ext, file_no_ext, args)
   elseif filetype == "cpp" then
     cmd = string.format(
-      "g++ -g -std=c++23 -Wall -Wextra -Wpedantic -fsanitize=address,undefined '%s' -o '%s' && '%s' %s",
+      "g++ -O1 -g -std=c++23 -Wall -Wextra -Wpedantic -fsanitize=address,undefined '%s' -o '%s' && '%s' %s",
       file,
       file_no_ext,
       file_no_ext,
